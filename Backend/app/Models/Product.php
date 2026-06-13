b71bd905153c297318f6d5a0c6f78a09f2ad852a
lysarl-24 <ly.sarl@student.passerellesnumeriques.org>
2026-06-13 10:41:53 +0700

    created migration and model

---
+++ Backend/app/Models/Product.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'sku',
+        'description',
+        'price',
+        'cost_price',
+        'stock_quantity',
+        'status'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+    public function inventoryTransactions()
+    {
+        return $this->hasMany(InventoryTransaction::class);
+    }
+}
