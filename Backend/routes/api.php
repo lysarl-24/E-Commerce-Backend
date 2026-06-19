4a10d5856c93aa8cc6e8b1449f5ee467bb3cb62a
lysarl-24 <ly.sarl@student.passerellesnumeriques.org>
2026-06-19 10:08:03 +0700

    deleted some crud

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,11 +1,6 @@
 <?php
 
 use App\Http\Controllers\Api\CategoryController;
-use App\Http\Controllers\Api\CustomerController;
-use App\Http\Controllers\Api\InventoryTransactionController;
-use App\Http\Controllers\Api\OrderController;
-use App\Http\Controllers\Api\OrderItemController;
-use App\Http\Controllers\Api\PaymentController;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\ProductImageController;
 use Illuminate\Http\Request;
@@ -19,8 +14,4 @@ Route::get('/user', function (Request $request) {
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('products', ProductController::class);
 Route::apiResource('product-images', ProductImageController::class);
-Route::apiResource('customers', CustomerController::class);
-Route::apiResource('orders', OrderController::class);
-Route::apiResource('order-items', OrderItemController::class);
-Route::apiResource('payments', PaymentController::class);
-Route::apiResource('inventory-transactions', InventoryTransactionController::class);
+
